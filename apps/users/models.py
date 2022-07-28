c89c908e64f7cdda6ccf2802dc69876cfaef34df
Ridzen <ridze00@gmail.com>
2022-07-28 09:02:29 -0500

    asd

--- apps/users/models.py
+++ apps/users/models.py
@@ -50,6 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
