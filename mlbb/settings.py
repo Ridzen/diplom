7c74c72d203b1619aaa3bdf2b4d909277abb388e
Akbar Maloer (ridzen) <93141972+Ridzen@users.noreply.github.com>
2022-08-01 09:06:50 -0500

    Update settings.py

--- mlbb/settings.py
+++ mlbb/settings.py
@@ -157,9 +157,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
-    BASE_DIR / 'assets',
+    BASE_DIR / 'static',
 ]
 
 MEDIA_URL = '/media/'
