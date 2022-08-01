e3be044a959b43a1ebf3d2368c49cecbea787d1b
Akbar Maloer (ridzen) <93141972+Ridzen@users.noreply.github.com>
2022-08-01 08:30:09 -0500

    Update settings.py

--- mlbb/settings.py
+++ mlbb/settings.py
@@ -157,6 +157,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
