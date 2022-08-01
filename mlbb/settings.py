5c5dedaf88740baae1445676f50346e25f61f688
Akbar Maloer (ridzen) <93141972+Ridzen@users.noreply.github.com>
2022-08-01 09:13:04 -0500

    Update settings.py

--- mlbb/settings.py
+++ mlbb/settings.py
@@ -158,9 +158,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
