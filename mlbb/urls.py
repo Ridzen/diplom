3c917db1d7007070858d32fcb59634fc6f54158c
Akbar Maloer (ridzen) <93141972+Ridzen@users.noreply.github.com>
2022-08-05 08:36:11 -0500

    Update urls.py

--- mlbb/urls.py
+++ mlbb/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include, re_path
-from gunicorn.app.pasterapp import serve
+from django.views.static import serve
 
 from .yasg import urlpatterns as doc_urls
 from . import settings
