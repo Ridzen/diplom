353ec04e53771857d3915bbcb7d53c01a387c04c
Ridzen <ridze00@gmail.com>
2022-08-09 04:44:55 -0500

    testing of code with secret key

--- mlbb/settings.py
+++ mlbb/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-k^yxx8!fe#bt+k&e_kg0q%(%#wfrt8vrdr84(@sh1r4!oh^i$s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG'))
