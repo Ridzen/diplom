a8d3088ae97c8d214ad322358fbb1be7024e2cb1
Ridzen <ridze00@gmail.com>
2022-08-11 09:03:23 -0500

    added more cors methods

--- mlbb/settings.py
+++ mlbb/settings.py
@@ -127,6 +127,29 @@ DATABASES = {
     }
 }
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
